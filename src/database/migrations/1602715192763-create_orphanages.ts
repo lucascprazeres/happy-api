712b99c4b28eb169d7ca71a7de5c21468588daf3
Lucas Prazeres <lucas.prazeres@castgroup.com.br>
2020-10-14 20:29:13 -0300

    feat: implements orphanage creation

--- src/database/migrations/1602715192763-create_orphanages.ts
+++ src/database/migrations/1602715192763-create_orphanages.ts
@@ -38,6 +38,10 @@ export class createOrphanages1602715192763 implements MigrationInterface {
           type: 'text'
         },
         {
+          name: 'opening_hours',
+          type: 'varchar'
+        },
+        {
           name: 'open_on_weekends',
           type: 'boolean',
           default: false
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,40 @@
 import express from 'express';
+import { getRepository } from 'typeorm';
 
 import './database/connection';
+
+import Orphanage from './models/Orphanages';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/users', (request, response) => {
-  return response.json({ message: 'it is working' });
+app.post('/orphanages', async (request, response) => {
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    open_on_weekends,
+    opening_hours
+  } = request.body;
+
+  const orphanagesRepository = getRepository(Orphanage);
+
+  const orphanage = await orphanagesRepository.create({
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    open_on_weekends,
+    opening_hours
+  });
+
+  await orphanagesRepository.save(orphanage);
+  
+  return response.json(orphanage);
 })
 
 app.listen(3333);
