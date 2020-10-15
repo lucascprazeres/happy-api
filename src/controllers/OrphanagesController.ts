369832e95ff376a7f8f0b0da3805d1deb7715f95
Lucas Prazeres <lucas.prazeres@castgroup.com.br>
2020-10-14 22:14:35 -0300

    feat: implements validation

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -2,6 +2,8 @@ import { Request, Response } from 'express';
 
 import { getRepository } from 'typeorm';
 
+import * as Yup from 'yup';
+
 import Orphanage from '../models/Orphanage';
 
 import orphanageView from '../views/orphanages_view';
@@ -46,9 +48,9 @@ export default {
     
     const images = requestImages.map(image => {
       return { path: image.filename }
-    })
+    });
 
-    const orphanage = await orphanagesRepository.create({
+    const data = {
       name,
       latitude,
       longitude,
@@ -57,7 +59,28 @@ export default {
       open_on_weekends,
       opening_hours,
       images
+    };
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      latitude: Yup.number().required(),
+      longitude: Yup.number().required(),
+      about: Yup.string().required(),
+      instructions: Yup.string().required(),
+      opening_hours: Yup.string().required(),
+      open_on_weekends: Yup.boolean().required(),
+      images: Yup.array(
+        Yup.object().shape({
+          path: Yup.string().required()
+        })
+      ),
     });
+
+    await schema.validate(data, {
+      abortEarly: false,
+    });
+
+    const orphanage = orphanagesRepository.create(data);
   
     await orphanagesRepository.save(orphanage);
     
